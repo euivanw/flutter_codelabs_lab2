5922dc022274702564ebbea7ba9a66a3d303dd6e
Ivan Wilhelm <ivan.whm@icloud.com>
2020-05-12 18:51:05 -0300

    Lab2: Criando o aplicativo inicial

---
+++ README.md
@@ -0,0 +1,11 @@
+# Escreva seu primeiro aplicativo Flutter, parte 2
+
+Este projeto contém o código fonte codelab [Write your first Flutter app, part 2](https://codelabs.developers.google.com/codelabs/first-flutter-app-pt2/#0) que foi traduzido por mim e disponibilizado em [Escreva seu primeiro aplicativo Flutter, parte 2](https://ivanwhm.gitbook.io/laboratorios-de-codigo-do-flutter/escreva-seu-primeiro-aplicativo-flutter-parte-2/introducao).
+
+Todo o código foi adaptado para que você possa ter a melhor experiência em aprender Flutter sem a barreira do idioma Inglês.
+
+Acompanhe todos os laboratórios de código Flutter traduzidos no endereço [https://ivanwhm.gitbook.io/laboratorios-de-codigo-do-flutter/](https://ivanwhm.gitbook.io/laboratorios-de-codigo-do-flutter/)
+
+Feito com &hearts; por Ivan Wilhelm.
+
+Codificado no [Visual Studio Code](https://code.visualstudio.com) da [Microsoft](https://www.microsoft.com.br)
---
+++ lib/main.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Bem-vindo(a) ao Flutter',
+      home: PalavrasRandomicas(),
+    );
+  }
+}
+
+class PalavrasRandomicas extends StatefulWidget {
+  @override
+  PalavrasRandomicasState createState() => PalavrasRandomicasState();
+}
+
+class PalavrasRandomicasState extends State<PalavrasRandomicas> {
+  final List<WordPair> _sugestoes = <WordPair>[];
+  final TextStyle _tamanhoDaFonte = const TextStyle(fontSize: 18);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Bem-vindo(a) ao Flutter'),
+      ),
+      body: _construirSugestoes(),
+    );
+  }
+
+  Widget _construirSugestoes() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16),
+      // A função de retorno itemBuild é chamada uma vez para cada
+      // par de sugestão de palavras e coloca cada sugestão dentro
+      // de uma linha do ListView. Nas linhas pares, a função adiciona
+      // uma linha na ListView para o par de palavras. Para as linhas
+      // ímpares, a função adiciona um widget chamado Divider para
+      // você visualizar um separador entre os pares de palavras.
+      // Perceba que o divisor pode ser difícil de ver em
+      // dispositivos com telas menores.
+      itemBuilder: (BuildContext _context, int i) {
+        // Adiciona um dividor de um pixel antes de cada
+        // linha da ListView.
+        if (i.isOdd) {
+          return Divider();
+        }
+
+        // A sintaxe "i ~/ 2" efetua a divisão do valor da
+        // variável i por 2 e retorna um resultado inteiro.
+        // Por exemplo: 1, 2, 3, 4, 5 passam a ser 0, 1, 1, 2, 2.
+        // Isto calcula o número corrente de pares de palavras
+        // na ListView, menos os widgets divisores.
+        final int index = i ~/ 2;
+        // Se você já chegou ao fim da lista de pares de
+        // palavras disponíveis...
+        if (index >= _sugestoes.length) {
+          // ... então são gerados mais 10 pares de palavras
+          // e adicionados na lista de sugestão.
+          _sugestoes.addAll(generateWordPairs().take(10));
+        }
+        return _construirLinha(_sugestoes[index]);
+      },
+    );
+  }
+
+  Widget _construirLinha(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _tamanhoDaFonte,
+      ),
+    );
+  }
+}
