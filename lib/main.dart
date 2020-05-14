976ebb0d67b4790de4dd73e3174637e31ef8c14e
Ivan Wilhelm <ivan.whm@icloud.com>
2020-05-14 19:16:39 -0300

    Lab2: Mudar a interface usando temas

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Bem-vindo(a) ao Flutter',
+      theme: ThemeData(
+        primaryColor: Colors.white,
+      ),
       home: PalavrasRandomicas(),
     );
   }
